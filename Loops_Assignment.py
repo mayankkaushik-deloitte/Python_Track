f0756262707a37cf2bf37cb7e4451b7f16efdb65
Mayank Kaushik <mayakaushik187@gmail.com>
2022-04-05 16:20:20 +0530

    Add the patterns

--- Loops_Assignment.py
+++ Loops_Assignment.py
@@ -1,8 +1,10 @@
 class Loops:
     val = 0
+
     def __init__(self, val):
         self.val = val
-    def print_pascals_traingle(self):
+
+    def print_pascals_triangle(self):
         # 1 0 0 0
         # 1 1 0 0
         # 1 2 1 0
@@ -19,7 +21,36 @@ class Loops:
             for j in range(cols):
                 print(arr[i][j], end='')
             print()
+    def first_pattern(self):
+        arr = [[0 for i in range(self.val)] for j in range(self.val)]
+        n = self.val
+        start = 1
+        for i in range(int(n / 2)):
+            for j in range(int(n / 2) - i):
+                print(" ", end = '')
+            for j in range(start):
+                print("*", end = '')
+            start = start + (2 if n % 2 == 1 else 1)
+            print()
+        for i in range(n):
+            print("*", end = '')
+        print()
+        start -= 2
+        for i in range(int(n / 2) - (1 if n % 2 == 1 else 0), -1, -1):
+            for j in range(int(n / 2) - i):
+                print(" ", end = "")
+            for j in range(start):
+                print("*", end = "")
+            start = start - (2 if n % 2 == 1 else 1)
+            print()
+
+    def second_pattern(self):
+        
+
 
 
 LoopOBJ = Loops(5)
-LoopOBJ.print_pascals_traingle()
+# Pascals part
+LoopOBJ.print_pascals_triangle()
+LoopOBJ.first_pattern()
+
