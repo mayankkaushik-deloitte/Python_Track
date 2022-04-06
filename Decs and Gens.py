251c7cfef75ac3267dc9acce5d8438604cd0f193
Mayank Kaushik <mayakaushik187@gmail.com>
2022-04-06 16:58:04 +0530

    Added the second problem for gens

--- Decs and Gens.py
+++ Decs and Gens.py
@@ -10,3 +10,24 @@ def multiply(a,b):
 multiply(2,4)
 
 #============GENERATORS===============
+def fibo(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        yield 0
+        yield 1
+
+    first = 0
+    yield first
+    second = 1
+    yield second
+    count = 3
+    while count <= n:
+        next = first + second
+        yield next
+        first = second
+        second = next
+        count = count + 1
+values = fibo(50)
+for i in values:
+    print(i)
