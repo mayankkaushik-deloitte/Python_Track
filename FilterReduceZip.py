887a0dc8aec39b53a961aa22c075470563353101
Mayank Kaushik <mayakaushik187@gmail.com>
2022-04-06 12:11:25 +0530

    Added the third problem

--- FilterReduceZip.py
+++ FilterReduceZip.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class FRZ:
     def __init__(self):
         pass
@@ -10,6 +13,16 @@ class FRZ:
         lst1 = [-1000, 500, -600, 700, 5000, -90000, -17500]
         ans = list(map(lambda x : -x,list(filter((lambda x : x < 0), lst1))))
         print(ans)
+    def second_problem(self):
+        lst1 = [-1000, 500, -600, 700, 5000, -90000, -17500]
+        print(reduce(lambda x, y : x * y, lst1))
+    def third_problem(self):
+        lst1 = ["Netflix", "Hulu", "Sling", "Hbo"]
+        lst2 = [198, 166, 237, 125]
+        zipAns = zip(lst1,lst2)
+        print(dict(zipAns))
 
 obj = FRZ()
 obj.first_problem()
+obj.second_problem()
+obj.third_problem()
