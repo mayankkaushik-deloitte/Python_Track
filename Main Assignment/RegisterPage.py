c734f072ea09d0beb8d0b3e49a74d65c68a0bccb
Mayank Kaushik <mayakaushik187@gmail.com>
2022-04-07 13:31:25 +0530

    Added the login functionality

---
+++ Main Assignment/RegisterPage.py
@@ -0,0 +1,6 @@
+class RegisterPage():
+    def __init__(self):
+        pass
+    def show(self):
+        print("The r page")
+obj = RegisterPage()
--- Main Assignment/main.py
+++ Main Assignment/main.py
@@ -1,24 +1,43 @@
+import sys
+sys.path.append("C:\\Users\\mayakaushik\\Python Track\\Main Assignment\\LoginPage.py")
+sys.path.append("C:\\Users\\mayakaushik\\Python Track\\Main Assignment\\RegisterPage.py")
+import LoginPage
+import RegisterPage
+
+
 class MAIN():
     option = 0
+
     def __init__(self):
         pass
+
     def welcome(self):
         print("*************WELCOME to BOOKMYSHOW*************")
+
     def showOptions(self):
-        self.welcome()
         print("1. Login")
         print("2. Register")
         print("3. Exit")
+
     def askForInput(self):
         self.option = int(input("Please enter one of the above options :: "))
         return self.option
 
+
 process = MAIN()
+process.welcome()
 process.showOptions()
 option = process.askForInput()
 while option != 3:
     if option == 1:
-        
+        LoginPage.obj.get_existing_users()
+        if LoginPage.obj.checkIfExists():
+            print("You are logged in!")
+        else:
+            print("Try signing in first")
+
     else:
-
-
+        RegisterPage.obj.show()
+    process.showOptions()
+    option = process.askForInput()
+print("Thanks you!")
