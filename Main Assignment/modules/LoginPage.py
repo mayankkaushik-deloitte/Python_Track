479e513ccc8ed3fd4e9bb790d94c263d71d99859
mayankkaushik-deloitte <101085397+mayankkaushik-deloitte@users.noreply.github.com>
2022-04-07 23:31:33 +0530

    Delete Main Assignment directory

--- Main Assignment/modules/LoginPage.py
+++
@@ -1,5 +0,0 @@
-class LoginPage():
-    def __init__(self):
-        pass
-    def loginShow(self,movies,users):
-        print()
--- Main Assignment/modules/RegisterPage.py
+++
@@ -1,5 +0,0 @@
-class RegisterPage():
-    def __init__(self):
-        pass
-    def registerShow(self,movies,users):
-        print()
